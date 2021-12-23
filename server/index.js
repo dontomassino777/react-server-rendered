a486a61b19f254db856b77221807c73f86e1ff88
Tomas Fisher <dontomassino777@gmail.com>
2021-12-23 10:46:28 -0700

    initial commit

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+
+const app = new express();
+
+app.get('/', (_req, res) => {
+    res.send(
+        `<h1>REACT ROCKS!</h1>`
+    )
+});
+
+
+app.listen(7777);
+console.info("Server is listening")
