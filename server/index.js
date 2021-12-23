becf1be374caf9b84c12fd7bbda096415771bade
Tomas Fisher <dontomassino777@gmail.com>
2021-12-23 11:23:09 -0700

    main react component renders

--- server/index.js
+++ server/index.js
@@ -1,13 +1,21 @@
-const express = require('express')
+// const express = require('express')
+
+import express from 'express';
+import { readFileSync } from 'fs';
 
 const app = new express();
 
-app.get('/', (_req, res) => {
-    res.send(
-        `<h1>REACT ROCKS!</h1>`
-    )
-});
+app.use(express.static("dist"));
 
+app.get('/', async (_req, res) => {
+    
+    const index = readFileSync(`public/index.html`, `utf8`);
+    res.send(index);
+    
+    // res.send(
+    //     `<h1>REACT ROCKS!</h1>`
+    // )
+});
 
 app.listen(7777);
 console.info("Server is listening")
